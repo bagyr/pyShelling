364f85e1887602fbc082a3f0799dc4d270beb7a8
bagyr <bagyrthelancer@gmail.com>
2013-05-14 15:25:14 +0400

    Many changes

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ import random
 
 class Field():
     def __init__(self, sizeX, sizeY, states):
+        """
+
+        :param sizeX: Field width
+        :param sizeY: Field height
+        :param states: Number of states
+        :return : Field
+        """
         self.sizeX = sizeX
         self.sizeY = sizeY
         self.numStates = states
@@ -27,6 +34,8 @@ class Field():
     def normalizeCords(self, x, y):
         """
 
+
+        :rtype : (int, int)
         :param x: Coordinate
         :param y: Coordinate
         :return: Real coords
@@ -49,7 +58,8 @@ class Field():
         :param y: Y cell
         :return: Cell content
         """
-        return self.arr[x][y]
+        realX, realY = self.normalizeCords(x, y)
+        return self.arr[realX][realY]
 
     def getWithOffset(self, x, y, dx, dy):
         """
@@ -61,19 +71,24 @@ class Field():
         :param dy: Y offset
         :return: Cell content
         """
-        realX, realY = x + dx, y + dy
-        while realX > self.sizeX:
-            realX -= self.sizeX
-        while realX < 0:
-            realX += self.sizeX
-        while realY > self.sizeY:
-            realY -= self.sizeY
-        while realY < 0:
-            realY += self.sizeY
+        realX, realY = self.normalizeCords(x + dx, y + dy)
         return self.arr[realX][realY]
 
     def set(self, x, y, val):
-        self.arr[x][y] = val
+        realX, realY = self.normalizeCords(x, y)
+        self.arr[realX][realY] = val
+
+    def getColor(self, x, y):
+        """
+
+        :rtype : pygame.Color
+        :param x: Cell x
+        :param y: Cell y
+        :return: Cell color
+        """
+        realX, realY = self.normalizeCords(x, y)
+        return self.colors[self.arr[realX][realY]]
+
 
 
 class Main():
@@ -82,28 +97,32 @@ class Main():
         stub
 
         """
-        self.fieldSize = (200, 200)
+        # self.fieldSize = (200, 200)
+        # self.numStates = 5
+        # self.field = [[random.randint(0, self.numStates - 1) for _ in xrange(self.fieldSize[0])]
+        #               for _ in xrange(self.fieldSize[1])]
+        self.field = Field(3, 3, 5)
         self.windowSize = (640, 480)
-        self.numStates = 5
-        self.field = [[random.randint(0, self.numStates - 1) for _ in xrange(self.fieldSize[0])]
-                      for _ in xrange(self.fieldSize[1])]
 
     def draw(self):
-        cellSize = min([(lambda i, j: i / j)(a, b) for (a, b) in zip(self.windowSize, self.fieldSize)])
-        outSurf = pygame.Surface((self.fieldSize[0] * cellSize, self.fieldSize[1] * cellSize))
-        for x in self.fieldSize[0]:
-            for y in self.fieldSize[1]:
-                pygame.draw.rect(outSurf, self.colors[self.field[x][y]],
+        cellSize = min([(lambda i, j: i / j)(a, b) for (a, b) in
+                        zip(self.windowSize, (self.field.sizeX, self.field.sizeY))])
+        outSurf = pygame.Surface((self.field.sizeX * cellSize, self.field.sizeY * cellSize))
+        for x in self.field.sizeX:
+            for y in self.field.sizeY:
+                pygame.draw.rect(outSurf, self.field.getColor(x, y),
                                  pygame.Rect(x * cellSize, y * cellSize, cellSize, cellSize))
         return outSurf
 
     def process(self):
-        for x in xrange(self.fieldSize[0]):
-            for y in xrange(self.fieldSize[1]):
-                if x == 0 and y == 0:
-                    neib = [self.field[1][0], self.field[0][1], self.field[1][1]]
-                elif x == 0:
-                    neib = [self.field[0][y + 1], self.field[0][y - 1], self.field[1][1]]
+        for x in xrange(self.field.sizeX):
+            for y in xrange(self.field.sizeY):
+                neib = []
+                for xOff in [-1, 0, 1]:
+                    for yOff in [-1, 1]:
+                        neib.append(self.field.get(x + xOff, y + yOff))
+                print(neib)
+
 
     def go(self):
         fpsClock = pygame.time.Clock()
@@ -127,4 +146,5 @@ class Main():
 
 if __name__ == "__main__":
     main = Main()
+    main.process()
     main.go()
